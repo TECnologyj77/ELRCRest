8ddd4c1e17d38ec81b91f428c4cfca5b0ee89fd0
theNateIrwin <Nathan.Irwin@stu.laroche.edu>
2017-02-22 11:19:20 -0500

    Updated newPodcastController post request and created a podcast class that stores and returns specific podcast data

--- src/main/java/hello/NewPodcastController.java
+++ src/main/java/hello/NewPodcastController.java
@@ -1,13 +1,13 @@
 package hello;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.concurrent.atomic.AtomicLong;
-
-/**
- * Created by Thomas on 2/4/2017.
- */
 
 @RestController
 public class NewPodcastController {
@@ -19,4 +19,11 @@ public class NewPodcastController {
         public NewPodcast newPodcast() {
             return null;
         }
+
+    @RequestMapping(value = "/admin/newPodcast", method = RequestMethod.POST)
+    public HttpStatus addNewPodcast(@RequestBody final Podcast podcast, HttpServletResponse response) {
+        System.out.println(podcast.getTitleOfPodcast() + " " + podcast.getPodcastURL() + " " + podcast.getPodcastDescription() + " " + podcast.getShowID());
+        response.setStatus(HttpServletResponse.SC_OK);
+        return HttpStatus.OK;
+    }
 }
