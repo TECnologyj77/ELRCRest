d19ed38e432dbe56875b4c1133ca70f2a7eeb8f1
TECnologyj77 <thomas.carney007@gmail.com>
2017-03-30 14:04:00 -0700

    Accessing Database

--- src/main/java/hello/ArtistController.java
+++ src/main/java/hello/ArtistController.java
@@ -4,13 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.*;
 
 import java.util.ArrayList;
+
+
 
 
 @RestController
 public class ArtistController {
 
+	private String name;
+	private String desc;
+	private String dj_name;
+	private String image;
 	@Autowired
 	private ArtistDao artistDao;
 
@@ -19,24 +26,51 @@ public class ArtistController {
 									@RequestParam(value = "artist_desc", defaultValue = "none") String artist_desc,
 									@RequestParam(value = "DJ_name", defaultValue = "none") String DJ_name,
 									@RequestParam(value = "DJ_desc", defaultValue = "none") String DJ_desc,
-									@RequestParam(value = "DJ_image", defaultValue = "none") String DJ_image) {
+									@RequestParam(value = "DJ_image", defaultValue = "none") String DJ_image) throws SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/elrc_radio?useSSL=false", "root", "admin");
+		//SQL query
+		String query = "SELECT * FROM artists";
+        Statement st = conn.createStatement();
 
+        getArtistList();
+
+        ResultSet rs = st.executeQuery(query);
+		int i = 0;
+        while(rs.next() || i < getArtistList().size() ){
+
+        	 name = rs.getString("artist_name");
+        	 desc = rs.getString("artist_desc");
+        	 dj_name = rs.getString("DJ_name");
+        	 image = rs.getString("DJ_image");
+        	 i++;
+			System.out.printf("%d,%s, %s, %s, %s\n", i, name, desc, dj_name, image);
+		}
+
+
+		return null;
+	}
+
+	public ArrayList<Artist> getArtistList() {
 
 		ArrayList<Artist> buildDBArtists = new ArrayList<Artist>();
+		buildDBArtists.clear();
 		buildDBArtists.add(new Artist("Greg 'the Professor' Lang", "Artist Description", "DJ Greg", "Greg's Description", "Bio Pic"));
 		buildDBArtists.add(new Artist("Ryan 'the Corporal' Couch", "Artist Description", "DJ Ryan", "Ryan's Description", "Bio Pic"));
 		buildDBArtists.add(new Artist("Tom 'the Cruise' Carney", "Artist Description", "DJ Tom", "Tom's Description", "Bio Pic"));
 		buildDBArtists.add(new Artist("Nate 'TwinTurbo' Irwin", "Artist Description", "DJ Nate", "Nate's Description", "Bio Pic"));
 		buildDBArtists.add(new Artist("Khalid 'the DJ' Fallatah", "Artist Description", "DJ Khalid", "Khalid's Description", "Bio Pic"));
 		buildDBArtists.add(new Artist("Kenny 'the Fresh' Swegman", "Artist Description", "DJ Kenny Fresh", "Kenny's Description", "Bio Pic"));
+		buildDBArtists.add(new Artist("Paul Blart", "Artist Description", "DJ Blart", "Paul's Description", "Bio Pic"));
 
 		for (int i = 0; i < buildDBArtists.size(); i++) {
 			artistDao.save(buildDBArtists.get(i));
 		}
 
+
 		return buildDBArtists;
 	}
 }
+
 
 /*
 	   Artist  getArtist = artistDao.findOne((long)3);
